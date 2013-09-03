2eff32521292e157d1fd931e28781a1ee52d24c1
Graham Jackson <graham.jackson07@gmail.com>
2013-09-02 22:20:29 -0400

    added css and new routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 HartlApp::Application.routes.draw do
+  get "users/new"
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
-   get "static_pages/contact"
+  get "static_pages/contact"
+
+  get 'static_pages/signup'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
